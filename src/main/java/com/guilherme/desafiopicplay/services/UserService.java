ffee933eea2f9dc4bfda3b3218657d028ab6d388
Guilherme Diniz <guilhermedinizlau@gmail.com>
2024-02-07 14:20:41 -0300

    UserController and UserService

---
+++ src/main/java/com/guilherme/desafiopicplay/services/UserService.java
@@ -0,0 +1,19 @@
+package com.guilherme.desafiopicplay.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.desafiopicplay.entities.User;
+import com.guilherme.desafiopicplay.repositories.UserRepository;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
